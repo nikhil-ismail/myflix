fecbf29757a2fc30fc1bdf3b324652b1fc21f9f3
Nikhil Ismail <nikhil.ismail20@gmail.com>
2022-06-29 23:24:20 -0700

    working on trending bug

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -5,24 +5,6 @@ import MyList from '../MyList/MyList';
 const Trending = (props) => {
 
     const { trending } = props;
-    let uniqueTrending = [];
-    let count = 0;
-
-    for (let i = 0; i < trending.length; i++) {
-        for (let j = 0; j < trending.length; j++) {
-            if (trending[i].movie === trending[j].movie) {
-                count++;
-            }
-        }
-        if (count <= 1) {
-            uniqueTrending.push(trending[i]);
-        }
-        count = 0;
-    }
-
-    console.log("data-----", trending);
-    console.log("unique-----", uniqueTrending);
-
 
     const [resultCount, setResultCount] = useState(5);
 
@@ -36,17 +18,17 @@ const Trending = (props) => {
             <Heading ml="80px" fontSize="22px">Trending {props.title}</Heading>
             <Flex backgroundColor="lightgray" flexDirection="column" mt="15px" ml="30px" mr="30px" padding="20px" borderRadius="10px">
                 {
-                    uniqueTrending.length === 0
+                    trending.length === 0
                     ?
                     <Text mt="5px">There is nothing trending at the moment!</Text>
                     :
-                    uniqueTrending.slice(0,resultCount).map((item, index) => {
+                    trending.slice(0,resultCount).map((item, index) => {
                         return (
                             <MyList key={index} movie={item} />
                         )
                     })
                 }
-                {uniqueTrending.length > resultCount && <Button onClick={handleShowMore}>Show More</Button>}
+                {trending.length > resultCount && <Button onClick={handleShowMore}>Show More</Button>}
             </Flex>
         </Flex>
     );
